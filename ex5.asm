90c832bb313882d4f209670857204e6ca3be5898
IvriHikri <ivrihikri@campus.technion.ac.il>
2022-04-08 17:55:26 +0300

    FINISHED RATUV

--- ex5.asm
+++ ex5.asm
@@ -1,79 +1,7 @@
-.global main
-.section .data
-head: .quad node_0
-src: .quad 478
-dst: .quad 328
-node_0: 
- .quad 478
- .quad node_1
-node_1: 
- .quad 53
- .quad node_2
-node_2: 
- .quad 430
- .quad node_3
-node_3: 
- .quad 241
- .quad node_4
-node_4: 
- .quad 458
- .quad node_5
-node_5: 
- .quad 53
- .quad node_6
-node_6: 
- .quad 89
- .quad node_7
-node_7: 
- .quad 386
- .quad node_8
-node_8: 
- .quad 53
- .quad node_9
-node_9: 
- .quad 393
- .quad node_10
-node_10: 
- .quad 393
- .quad node_11
-node_11: 
- .quad 10
- .quad node_12
-node_12: 
- .quad 430
- .quad node_13
-node_13: 
- .quad 109
- .quad node_14
-node_14: 
- .quad 283
- .quad node_15
-node_15: 
- .quad 386
- .quad node_16
-node_16: 
- .quad 48
- .quad node_17
-node_17: 
- .quad 10
- .quad node_18
-node_18: 
- .quad 328
- .quad node_19
-node_19: 
- .quad 109
- .quad node_20
-node_20: 
- .quad 89
- .quad node_21
-node_21: 
- .quad 53
- .quad node_22
-node_22: 
- .quad 241
- .quad 0
+.global _start
+
 .section .text
-main:
+_start:
 
 movq (src), %rax #rax<-src
 movq (dst), %rbx #rbx<-dst
@@ -86,13 +14,16 @@ je end
 
 xor %rcx, %rcx #counter src
 xor %rdx, %rdx #counter dst
+xor %rdi, %rdi
+xor %rsp, %rsp
 
-#r10 <- adress of src
-#r11 <- adress of dst
-movq (%r12),%r13
-cmp %rax, %r13
+#r10 <- adress of src previous
+#r11 <- adress of dst previous
+#checking root
+movq (%r12),%r13 #r13<- head
+cmp %rax, %r13 #source==head?
 je visited_src_from_root
-cmp %rbx, %r13
+cmp %rbx, %r13 #dest==head?
 je visited_dst_from_root
 
 main_loop:
@@ -123,10 +54,22 @@ je next_update
 movq %r14, 8(%r11) #dst_previous->next= src
 
 next_update:
-movq 8(%r14), %rcx
-movq 8(%r15), %rdx
+movq 8(%r14), %rcx #source-> next goes to rcx
+movq 8(%r15), %rdx #dest-> next goes to rdx
 movq %rcx, 8(%r15)
 movq %rdx, 8(%r14)
+cmp $1, %rdi
+je update_dst_to_head
+cmp $1, %rsp
+je update_src_to_head
+jmp end
+
+update_dst_to_head:
+movq %r15, (head)
+jmp end
+
+update_src_to_head:
+movq %r14, (head)
 jmp end
 
 visited_src:
@@ -137,7 +80,8 @@ jmp advance_head
 
 update_src_adress:
 lea (%r12), %r10 #src_previous
-lea 8(%r12), %r14 #src
+lea 8(%r12), %r14
+movq (%r14), %r14 #src
 jmp advance_head
 
 visited_dst:
@@ -148,11 +92,13 @@ jmp advance_head
 
 update_dst_adress:
 lea (%r12), %r11 #dst_previous
-lea 8(%r12), %r15 #dst 
+lea 8(%r12), %r15
+movq (%r15), %r15 #dst 
 jmp advance_head
 
 visited_src_from_root:
 incq %rcx
+incq %rdi
 cmp $1, %rcx
 je update_src_adress_from_root
 jmp main_loop
@@ -164,6 +110,7 @@ jmp main_loop
 
 visited_dst_from_root:
 incq %rdx
+incq %rsp
 cmp $1, %rdx
 je update_dst_adress_from_root
 jmp main_loop
